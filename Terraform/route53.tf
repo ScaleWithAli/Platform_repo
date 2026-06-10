e2d7daac01e183c731d88badc6bc9136f40a85d8
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-10 10:47:17 +0500

    Update route53.tf

--- Terraform/route53.tf
+++ Terraform/route53.tf
@@ -3,10 +3,23 @@ resource "aws_route53_zone" "main" {
   tags = local.common_tags
 }
 
+resource "aws_route53_record" "main" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = helm_release.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname
+    zone_id                = data.aws_elb_hosted_zone_id.nlb.id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "wildcard" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "*.${var.domain_name}"
   type    = "A"
+
   alias {
     name                   = helm_release.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname
     zone_id                = data.aws_elb_hosted_zone_id.nlb.id
